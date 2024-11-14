3907e1883f31fb493c64c1cc90fb34cf2b86f622
shruti111995 <shrutidurbude@gmail.com>
2024-11-14 16:00:35 +0530

    shruti

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ class firstClass:
     val1=0
     val2=0
 
-print('firstClass',firstClass)
-print('firstClass.val1',firstClass.val1)
-print('firstClass.val2',firstClass.val2)
+f1=firstClass()
+
+print('f1.val1=',f1.val1)
+print('f2.val2=',f2.val2)
