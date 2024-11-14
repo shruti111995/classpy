04cfc35b21ad33dd9276b8dbe5516157f98c4b04
shruti111995 <shrutidurbude@gmail.com>
2024-11-14 16:31:32 +0530

    shruti

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ class firstClass:
     val1=0
     val2=0
 
-f1=firstClass()
+    def __init__(self,a,b):
+        print('thisis constructor')
+        self.val1=a
+        self.val2=b
+        
+    def addValue(self):
+        print(f'addition={self.val1+self.val2}')    
+
+f1=firstClass(2,3)
+f1.addValue()
 
 print('f1.val1=',f1.val1)
-print('f2.val2=',f2.val2)
+print('f1.val2=',f1.val2)
+f2=firstClass(10,20)
+f2.addValue()
